6151c11f5a0d09b2fd38d7d82291c82b9ae9f965
Lohith-Kumar-Nallamothu <67246428+Lohith-Kumar-Nallamothu@users.noreply.github.com>
2020-06-24 21:57:08 +0530

    Add files via upload

---
+++ Inheritance/Person5.java
@@ -0,0 +1,6 @@
+public class Person5 {
+	String name;
+	void set(String n){
+		name = n;
+	}
+}
---
+++ Inheritance/TestEmployee53.java
@@ -0,0 +1,12 @@
+public class TestEmployee53 {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setName("Lohith");
+		emp.setYearOfWorkStarted(2020);
+		emp.setAnnualSalary(4.5);
+		emp.setNationalInsuranceNumber("BN12FD953");
+		emp.getEmployeedetails();
+	}
+
+}
