b17564fe757140d1ccb7303bdc4e6a1072f08268
Lohith-Kumar-Nallamothu <67246428+Lohith-Kumar-Nallamothu@users.noreply.github.com>
2020-06-24 22:25:20 +0530

    Add files via upload

---
+++ OverRidePoly/Override.java
@@ -0,0 +1,39 @@
+class Fruit {
+	String name;
+	String taste;
+	String size;
+	Fruit(String n,String t,String s){
+		name = n;
+		taste = t;
+		size = s;
+	}
+	public void eat() {
+		System.out.println(name+" is "+taste);
+	}
+}
+class Apple extends Fruit {
+
+	Apple(String n,String t,String s){
+		super(n,t,s);
+	}
+	public void eat() {
+		System.out.println(name+" is "+taste);
+	}
+}
+class Orange extends Fruit {
+
+	Orange(String n,String t,String s){
+		super(n,t,s);
+	}
+	public void eat() {
+		System.out.println(name+" is "+taste);
+	}
+}
+public class Override {
+public static void main(String[] args) {
+	Apple a = new Apple("apple","sweet","medium");
+	Orange o = new Orange("orange","sour","small");
+	a.eat();
+	o.eat();
+}
+}
